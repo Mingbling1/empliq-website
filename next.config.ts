cfdad26286576bfafb0247a31d27e27ff161b602
Mingbling1 <jimmy.auris@outlook.com>
2026-02-27 17:08:25 -0500

    feat: add CF Workers deps, package-lock.json, update OpenNext config for Cloudflare deployment

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,4 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
 };
 
-// Note: When @opennextjs/cloudflare is installed, uncomment this for local dev:
-// import("@opennextjs/cloudflare").then(({ initOpenNextCloudflareForDev }) => {
-//   initOpenNextCloudflareForDev();
-// }).catch(() => {});
-
 export default nextConfig;
